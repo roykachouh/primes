d2e8ca32ac6b8fd8a8e230d3cc97731cc4a75634
Kachouh <rkkacho@8c8590230d87.ant.amazon.com>
2017-11-14 21:44:26 -0800

    Add simple prime calculator

---
+++ src/main/kotlin/PrimeTester.kt
@@ -0,0 +1,35 @@
+class PrimeTester : Thread() {
+    override fun run() {
+        while (true) {
+            for (i in 3..10000) {
+                var isPrime = true
+                var j: Long = 2
+                while (j <= i / 2 && isPrime) {
+                    isPrime = i % j > 0
+                    j++
+                }
+                if (isPrime) {
+                    println("Found prime: " + i)
+                }
+            }
+        }
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun main(a: Array<String>) {
+            var frequency: Long = 1
+
+            val primeTester = PrimeTester()
+            primeTester.start()
+            while (true) {
+                try {
+                    Thread.sleep(frequency * 500)
+                } catch (e: InterruptedException) {
+                    println("Thread interrupted")
+                }
+            }
+        }
+    }
+}
